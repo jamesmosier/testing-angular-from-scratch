4ef971aa92a36810d2084bfaf32c5f81446950e0
James Mosier <jamesdmosier@gmail.com>
2015-06-08 16:13:25 -0400

    added beforeEach to house common code

--- spec/ExpenseSpec.js
+++ spec/ExpenseSpec.js
@@ -1,7 +1,19 @@
 describe("Expense objects", function(){
+
+    var expenseItem,
+        expense;
+
+    //put common code in here
+    beforeEach(function(){
+        expenseItem = new ExpenseItem(100);
+        expense = new Expense(expenseItem);
+    });
+
     it("should be of type ExpenseItem", function() {
-        var expenseItem = new ExpenseItem();
-        var expense = new Expense(expenseItem);
         expect(expense.expenseItem).toBe(expenseItem);
     });
+
+    it("should have the correct expense amount", function(){
+        expect(expense.expenseItem.amount).toEqual(100);
+    })
 });
